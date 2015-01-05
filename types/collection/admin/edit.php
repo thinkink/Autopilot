17b0d66657235c19cde65ad8ee0ad926ac7fc4f4
Artur <artur@agentejo.com>
2015-01-05 15:24:36 +0100

    edit collection

--- types/collection/admin/edit.php
+++ types/collection/admin/edit.php
@@ -3,7 +3,7 @@
     $collections = $app->db->find('common/collections')->toArray();
 %}
 
-<div class="uk-grid" data-uk-grid-margin>
+<div class="uk-grid" data-uk-grid-margin ng-controller="collectionMenuItem">
     <div class="uk-width-2-3">
         <div class="uk-form-row">
             <label><span class="uk-badge app-badge">@lang('Collection')</span></label>
@@ -13,6 +13,9 @@
                     <option value="{{ $collection['_id'] }}">{{ $collection['name'] }}</option>
                     @endforeach
                 </select>
+            </div>
+            <div class="uk-margin-small-top uk-text-small" ng-show="data.collectionId">
+                <a ng-click="manageCollection(data.collectionId)"><i class="uk-icon-list"></i> Manage items</a>
             </div>
         </div>
     </div>
@@ -25,3 +28,21 @@
         </div>
     </div>
 </div>
+
+<script>
+
+    (function($){
+
+        App.module.controller("collectionMenuItem", function($scope, $rootScope, $http, $timeout){
+
+            $scope.manageCollection = function(collectionId) {
+
+                var route = 'collections/entries/'+collectionId;
+
+                App.viewpopup(route);
+            };
+
+        });
+
+    })(jQuery);
+</script>
